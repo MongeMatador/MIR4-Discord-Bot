6fd6eb2e0656875f2ae47042fdfc08a6722643dc
MongeMatador <danielarampeloti@gmail.com>
2026-08-19 02:04:19 -0300

    Update panel_service.py

--- services/panel_service.py
+++ services/panel_service.py
@@ -105,4 +105,3 @@ class PanelService:
                         channel_id = EXCLUDED.channel_id,
                         message_id = EXCLUDED.message_id
                 """, guild_id, map_type, floor, dest_channel.id, new_msg.id)
-📄
